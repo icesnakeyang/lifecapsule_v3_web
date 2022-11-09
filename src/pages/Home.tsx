0956ab0678962a5fc1f045205944034240409a98
icesnakeyang <flow_ocean@yahoo.com>
2022-11-09 14:37:30 +0800

    progress

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,6 @@ import {
 import banner1 from "../resources/aser-min.jpg"
 
 const contentStyle: React.CSSProperties = {
-    height: '360px',
     color: '#912929',
     lineHeight: '360px',
     textAlign: 'center',
@@ -96,80 +95,87 @@ const Home = () => {
     };
 
     return (
-        <div
-            style={{}}
-        >
-            <Carousel autoplay>
+        <div>
+            <div
+                style={{height: '80vh', background: 'red'}}
+            >
+                <div style={{height:'50vh'}}>
+                    <Carousel autoplay style={{height: '50vh', background: 'green', padding: 20}}>
+                        {/*<div style={{height:'50vh',background:'yellowgreen', padding:20}}>*/}
+                        {/*    <div style={{background:'green', padding:20}}>*/}
+                        {/*        <img src={banner1} width="100%" height='50vh'/>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+                    </Carousel>
+                </div>
+
                 <div>
-                    <h3 style={contentStyle}>
-                        <img src={banner1} width="100%"/>
-                    </h3>
-                </div>
-            </Carousel>
-
-            <div>
-                <div
-                    style={{position: 'absolute', top: 120, left: 100, width: 340, padding: 20, background: '#333675'}}>
-                    <div style={{}}>
-                        <div style={{color: '#fff', fontSize: 24}}>
-                            {t("webSite.home.tip1")}
-                        </div>
-                        <div style={{color: '#fff', fontSize: 14, marginTop: 20}}>
-                            {t("webSite.home.tip2")}
-                        </div>
-                    </div>
-                </div>
-
-                <div style={{position: 'absolute', top: 280, left: 550, padding: 20, background: '#333675'}}>
-                    <div>
+                    <div
+                        style={{
+                            width: 340,
+                            padding: 20,
+                            background: '#333675'
+                        }}>
                         <div style={{}}>
                             <div style={{color: '#fff', fontSize: 24}}>
-                                {t("webSite.home.tip8")}
+                                {t("webSite.home.tip1")}
                             </div>
                             <div style={{color: '#fff', fontSize: 14, marginTop: 20}}>
-                                {t("webSite.home.tip3")}
+                                {t("webSite.home.tip2")}
                             </div>
-                            <div style={{color: '#fff', fontSize: 14}}>
-                                {t("webSite.home.tip4")}
-                            </div>
-                            <div style={{color: '#fff', fontSize: 14}}>
-                                {t("webSite.home.tip5")}
-                            </div>
-                            <div style={{color: '#fff', fontSize: 14}}>
-                                {t("webSite.home.tip6")}
-                            </div>
-                            <div style={{color: '#fff', fontSize: 14}}>
-                                {t("webSite.home.tip7")}
+                        </div>
+                    </div>
+
+                    <div style={{padding: 20, background: '#333675'}}>
+                        <div>
+                            <div style={{}}>
+                                <div style={{color: '#fff', fontSize: 24}}>
+                                    {t("webSite.home.tip8")}
+                                </div>
+                                <div style={{color: '#fff', fontSize: 14, marginTop: 20}}>
+                                    {t("webSite.home.tip3")}
+                                </div>
+                                <div style={{color: '#fff', fontSize: 14}}>
+                                    {t("webSite.home.tip4")}
+                                </div>
+                                <div style={{color: '#fff', fontSize: 14}}>
+                                    {t("webSite.home.tip5")}
+                                </div>
+                                <div style={{color: '#fff', fontSize: 14}}>
+                                    {t("webSite.home.tip6")}
+                                </div>
+                                <div style={{color: '#fff', fontSize: 14}}>
+                                    {t("webSite.home.tip7")}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div style={{marginTop: 50, marginLeft: 220}}>
-                <Button
-                    style={{background: '#333675', borderColor: '#333333'}}
-                    type="primary"
-                    onClick={() => {
-                        onSignIn();
-                    }}
-                    size='large'
-                >
-                    Free To Try
-                </Button>
+                <div style={{marginTop: 50, marginLeft: 220}}>
+                    <Button
+                        style={{background: '#333675', borderColor: '#333333'}}
+                        type="primary"
+                        onClick={() => {
+                            onSignIn();
+                        }}
+                        size='large'
+                    >
+                        Free To Try
+                    </Button>
+                </div>
             </div>
             <div style={{
+                height: '20vh',
                 background: '#ddd',
                 marginTop: 100,
-                height: 200,
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: "center"
             }}>
-                <div style={{margin:50}}>
+                <div style={{}}>
                     <a href="/user_privacy">User Private Policy</a>
                 </div>
-                <div style={{margin:50}}>
+                <div style={{marginLeft: 100}}>
                     <a href="">Copyright © GOGOYANG DATATECH LIMIT</a>
                 </div>
             </div>
